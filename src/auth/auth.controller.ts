d15416f0009dd9353f8e968b3d4346d78223cf6e
patthad <beebobbob956@gmai.com>
2025-04-19 22:28:48 +0700

    feat: 🎸 jwt login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { LoginDto } from './dto/create-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  create(@Body() createAuthDto: CreateAuthDto) {
+  create(@Body() createAuthDto: LoginDto) {
     return this.authService.login(createAuthDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { LoginDto } from './dto/create-auth.dto';
+import { LoginDto, SignUpDto } from './dto/create-auth.dto';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
-  login(createAuthDto: LoginDto) {
-    const user = this.prisma.user.findUnique({
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(createAuthDto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
       where: {
         username: createAuthDto.username,
         password: createAuthDto.password,
@@ -15,12 +19,40 @@ export class AuthService {
     if (!user) {
       throw new HttpException('Invalid credentials', 401);
     }
+    // Generate JWT token
+    const token = this.jwtService.sign({
+      username: user.username,
+      userId: user.id,
+      email: user.email,
+    });
+    return {
+      access_token: token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    };
   }
-  validateUser(userId: string) {
+  async validateUser(userId: string) {
     return this.prisma.user.findUnique({
       where: {
         id: userId,
       },
     });
+  }
+
+  async signUp(createAuthDto: SignUpDto) {
+    try {
+      const data = await this.prisma.user.create({
+        data: {
+          username: createAuthDto.username,
+          email: createAuthDto.email,
+          password: createAuthDto.password,
+        },
+      });
+      return { username: data.username, email: data.email };
+    } catch (error) {
+      throw new HttpException('User already exists', 409);
+    }
   }
 }
