8ee24913a8308629bcb8f15e0430242edfd4c3d2
patthad <beebobbob956@gmai.com>
2025-04-20 18:37:49 +0700

    feat: 🎸 cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://fe.21days.floggy.online'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
